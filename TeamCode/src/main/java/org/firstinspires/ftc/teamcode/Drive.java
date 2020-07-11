5c092999d6dd27044e5b6e48ade8afbb3a2ebd30
adamteaney <adamteaney@gmail.com>
2020-07-11 16:15:08 -0500

    Changed the name of the program on the phone hopefully!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp (name = "Adam's git commit -am \"Hopefully halved the speed.\"\nSlower Tank Drive")
+@TeleOp (name = "Adam's Slower Tank Drive")
 //@Disabled
 public class Drive extends OpMode {
     private ElapsedTime runtime = new ElapsedTime();
